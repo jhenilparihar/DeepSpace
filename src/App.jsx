b00fff1c5b5e8937f408d2de1258e1f14247a141
Jhenil Parihar <jhenilparihar@gmail.com>
2022-05-15 14:35:21 +0530

    NFT Details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './bootstrap/css/bootstrap.css';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./pages/Navbar/Navbar";
@@ -8,6 +9,7 @@ import Mint from "./pages/Create/mint";
 import Profile from "./pages/Profile/profile";
 import NoPage from './pages/NoPage/NoPage';
 import Settings from './pages/Profile/profile-setting';
+import NFTDetails from './pages/NFT-Details/NFT-Details';
 
 function App() {
   return (
@@ -20,6 +22,7 @@ function App() {
           <Route path="mint" element={<Mint />} />
           <Route path="profile" element={<Profile />} />
           <Route path="profile/settings" element={<Settings />} />
+          <Route path="assets/details" element={<NFTDetails />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
