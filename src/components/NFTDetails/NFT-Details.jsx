6d546438b741ec8d521280cfe133fc28ebd90c74
Jhenil Parihar <jhenilparihar@gmail.com>
2022-06-04 12:18:02 +0530

    styles updated

--- src/components/NFTDetails/NFT-Details.jsx
+++ src/components/NFTDetails/NFT-Details.jsx
@@ -5,7 +5,7 @@ import "./style.css";
 import eth from "./eth.svg";
 import wallet from "./wallet.png";
 import share from "./share.png";
-
+import content from "./content.png";
 class NFTDetails extends Component {
   constructor(props) {
     super(props);
@@ -23,19 +23,36 @@ class NFTDetails extends Component {
       <>
         <div class="details-main">
           <div className="row">
-            <div class="col col-sm-12 detail-page__nft-image">
-              <div className="row nft-image-header">
-                <img class="eth-small" src={eth} alt="" />
+            <div class="col col-sm-12 detail-page__nft-image_section">
+              <div className="row detail-page__nft-image">
+                <div className="row nft-image-header">
+                  <img class="eth-small" src={eth} alt="" />
+                </div>
+                <div className="row nft-image-div">
+                  <img
+                    src={
+                      this.props.NFT.metaData !== undefined
+                        ? this.props.NFT.metaData.imageUrl
+                        : ""
+                    }
+                    alt=""
+                  ></img>
+                </div>
               </div>
-              <div className="row nft-image-div">
-                <img
-                  src={
-                    this.props.NFT.metaData !== undefined
-                      ? this.props.NFT.metaData.imageUrl
-                      : ""
-                  }
-                  alt=""
-                ></img>
+              <div className="row price_row detail_section">
+                <div className="row created_on div_title">
+                  <img class="description_image" src={content} alt="" />
+                  Description
+                </div>
+                <div className="row price_div">
+                  <div className="row div_content_text description_content">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                  </div>
+                </div>
               </div>
             </div>
             <div class="col col-sm-12 nft-item-details">
@@ -116,34 +133,54 @@ class NFTDetails extends Component {
                             <p className="mt-2">Currently not for sale!</p>
                           </>
                         )
-                      ) : (
-                        <>
-                          <button disabled class="buy_now-btn">
-                            <img class="wallet_icon" src={wallet} alt=""></img>
-                            Buy now
-                          </button>
-                          <p />
-                        </>
-                      )}
+                      ) : null}
 
                       {this.props.accountAddress ===
                       this.props.NFT.currentOwner ? (
-                        <form
-                          onSubmit={(e) => {
-                            e.preventDefault();
-                            this.callChangeTokenPriceFromApp(
-                              this.props.NFT.tokenId.toNumber(),
-                              this.state.newNFTPrice
-                            );
-                          }}
-                        >
-                          <div className="form-group mt-4 ">
-                            <label htmlFor="newNFTPrice">
-                              <span className="font-weight-bold">
-                                Change Token Price
-                              </span>{" "}
-                              :
-                            </label>{" "}
+                        this.props.NFT.forSale ? (
+                          <button
+                            class="remove-btn"
+                            onClick={() =>
+                              this.props.toggleForSale(
+                                this.props.NFT.tokenId.toNumber()
+                              )
+                            }
+                          >
+                            Remove from sale
+                          </button>
+                        ) : (
+                          <button
+                            class="keep-btn"
+                            onClick={() =>
+                              this.props.toggleForSale(
+                                this.props.NFT.tokenId.toNumber()
+                              )
+                            }
+                          >
+                            Keep for sale
+                          </button>
+                        )
+                      ) : null}
+                    </div>
+                  </div>
+                </div>
+                {this.props.accountAddress === this.props.NFT.currentOwner ? (
+                  <div className="row price_row">
+                    <div className="row created_on div_title">
+                      Change Token Price :
+                    </div>
+                    <div className="row price_div change-price-row">
+                      <form
+                        onSubmit={(e) => {
+                          e.preventDefault();
+                          this.callChangeTokenPriceFromApp(
+                            this.props.NFT.tokenId.toNumber(),
+                            this.state.newNFTPrice
+                          );
+                        }}
+                      >
+                        <div className="form-group mt-4 ">
+                          <div class="input-group price-input_detail">
                             <input
                               required
                               type="number"
@@ -159,54 +196,63 @@ class NFTDetails extends Component {
                               }
                             />
                           </div>
-                          <button
-                            type="submit"
-                            style={{
-                              fontSize: "0.8rem",
-                              letterSpacing: "0.14rem",
-                            }}
-                            className="btn btn-outline-info mt-0 w-50"
-                          >
-                            change price
+                          <button type="submit" class="change_price-btn">
+                            Change Price
                           </button>
-                        </form>
-                      ) : null}
-
-                      {this.props.accountAddress ===
-                      this.props.NFT.currentOwner ? (
-                        this.props.NFT.forSale ? (
-                          <button
-                            className="btn btn-outline-danger mt-4 w-50"
-                            style={{
-                              fontSize: "0.8rem",
-                              letterSpacing: "0.14rem",
-                            }}
-                            onClick={() =>
-                              this.props.toggleForSale(
-                                this.props.NFT.tokenId.toNumber()
-                              )
-                            }
-                          >
-                            Remove from sale
-                          </button>
-                        ) : (
-                          <button
-                            className="btn btn-outline-success mt-4 w-50"
-                            style={{
-                              fontSize: "0.8rem",
-                              letterSpacing: "0.14rem",
-                            }}
-                            onClick={() =>
-                              this.props.toggleForSale(
-                                this.props.NFT.tokenId.toNumber()
-                              )
-                            }
-                          >
-                            Keep for sale
-                          </button>
-                        )
-                      ) : null}
+                        </div>
+                      </form>
                     </div>
+                  </div>
+                ) : null}
+
+                <div className="row price_row">
+                  <div className="row created_on">Token Details</div>
+                  <div className="row price_div">
+                    <div className="row div_content_text">
+                      <div className="col-6">Number Of Transfers</div>
+                      <div className="col-3">
+                        {this.props.NFT.numberOfTransfers.toNumber()}
+                      </div>
+                    </div>
+                    <div className="row div_content_text">
+                      <div className="col-6">Previous Owner</div>
+                      <div className="col-3">
+                        <a href="">
+                          {this.props.NFT.previousOwner.substr(0, 5) +
+                            "..." +
+                            this.props.NFT.previousOwner.slice(
+                              this.props.NFT.previousOwner.length - 5
+                            )}
+                        </a>
+                      </div>
+                    </div>
+                    <div className="row div_content_text">
+                      <div className="col-6">Owned By</div>
+                      <div className="col-3">
+                        <a href="">
+                          {this.props.NFT.currentOwner.substr(0, 5) +
+                            "..." +
+                            this.props.NFT.currentOwner.slice(
+                              this.props.NFT.currentOwner.length - 5
+                            )}
+                        </a>
+                      </div>
+                    </div>
+
+                    <div className="row div_content_text">
+                      <div className="col-6">Minted By</div>
+                      <div className="col-3">
+                        <a href="">
+                          {this.props.NFT.mintedBy.substr(0, 5) +
+                            "..." +
+                            this.props.NFT.mintedBy.slice(
+                              this.props.NFT.mintedBy.length - 5
+                            )}
+                        </a>
+                      </div>
+                    </div>
+
+                    <div className="row "></div>
                   </div>
                 </div>
               </div>
